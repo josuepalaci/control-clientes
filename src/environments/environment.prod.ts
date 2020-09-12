aac443deea4bb7a91b9d4ae0414553f76eb8b101
josue <josuepalaci@gmail.com>
2020-09-12 16:43:43 -0600

    primer avance

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firestore: {
+    apiKey: "AIzaSyDzEJLw_QRKfXWBdThJBxAzbZnn_V41H2U",
+    authDomain: "control-clientes-98ee3.firebaseapp.com",
+    databaseURL: "https://control-clientes-98ee3.firebaseio.com",
+    projectId: "control-clientes-98ee3",
+    storageBucket: "control-clientes-98ee3.appspot.com",
+    messagingSenderId: "260113145315",
+    appId: "1:260113145315:web:f0913bcd8d13f7aa6814d6",
+    measurementId: "G-159NKDMY5L"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firestore: {
+    apiKey: "AIzaSyDzEJLw_QRKfXWBdThJBxAzbZnn_V41H2U",
+    authDomain: "control-clientes-98ee3.firebaseapp.com",
+    databaseURL: "https://control-clientes-98ee3.firebaseio.com",
+    projectId: "control-clientes-98ee3",
+    storageBucket: "control-clientes-98ee3.appspot.com",
+    messagingSenderId: "260113145315",
+    appId: "1:260113145315:web:f0913bcd8d13f7aa6814d6",
+    measurementId: "G-159NKDMY5L"
+  }
 };
 
 /*
